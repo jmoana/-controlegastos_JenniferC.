bd7dfdd8a7eec9a1d58424e908403c8f31ec8c8e
Jmoana <jhenycruz7@gmail.com>
2021-02-23 10:25:32 -0300

    psi atividade

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista de gastos</title>
+</head>
+<body>
+    <h1>Lista de Gastos do Mês de Fevereiro</h1>
+   <table>
+       <tr>
+           <td>Datas:</td> <td>Descrição:<td></td>Valor:</td>
+       </tr>
+       <tr>
+           <td>01/02/2021</td> <td>Salgado<td></td>R$ 1,50</td>
+       </tr>
+       <tr>
+           <td>02/02/2021</td> <td>impressão de trabalho (10 páginas)<td></td>R$ 4,00</td>
+       </tr>
+   </table>
+</body>
+</html>
