cea3613ee0465657a4e1ac9ca0a7fd87248ac4a6
Jmoana <jhenycruz7@gmail.com>
2021-02-23 11:34:13 -0300

    ta terminando

--- application/views/nova_categoria.php
+++ application/views/nova_categoria.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>gastos</title>
+    <title>Nova Categoria</title>
 </head>
 <body>
-    <h1>novos gastos</h1>
+    <h1>Nova categoria</h1>
   <form action="">
-      <label for="data">Data<input type="date" name="data"></label> <br>
-      <label for="descricao">Descrição<input type="text" name="descricao"></label> <br>
-      <label for="valor">Valor<input type="number" name="valor"></label></form><br>
+    Nome: <input type="text" name="nome"></label> <br>
+    Descrição: <input type="text" name="descricao"></label> <br>
+
       <button type="button">Enviar</button>
       <button type="reset">Limpar </button>
 
