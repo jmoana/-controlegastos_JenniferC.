5fe9625154a3279429c39c64f52a7e9f71a9b70d
Jmoana <jhenycruz7@gmail.com>
2021-02-23 11:32:08 -0300

    atividade psi quase no fim

---
+++ application/views/listar_categorias.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista de categoria</title>
+</head>
+<body>
+    <h1>Lista de categorias</h1>
+    <ul>
+  <li>Transporte</li>
+  <li>Alimentação</li>
+  <li>Remédios</li>
+  <li>Telefone/Internet</li>
+  <li>bijuterias</li>
+</ul>  
+</body>
+</html>
